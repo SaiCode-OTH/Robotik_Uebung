d8eef477a108413c8f622a7b1bcbc8a188215709
Jaaa <you@example.com>
2024-04-09 10:56:58 +0200

    übung 1 aufgabe 2

--- main.c
+++ main.c
@@ -1,41 +1,44 @@
 #include "nnxt.h"
 
+void turnLeft () {
+    Motor_Drive(Port_A, Motor_dir_forward, 75);
+    Motor_Drive(Port_B, Motor_dir_backward, 75);
+    Delay(330);
+    Motor_Stop(Port_A, Motor_stop_float);
+    Motor_Stop(Port_B, Motor_stop_float);
+}
+void turnRight () {
+    Motor_Drive(Port_A, Motor_dir_backward, 75);
+    Motor_Drive(Port_B, Motor_dir_forward, 75);
+    Delay(330);
+    Motor_Stop(Port_A, Motor_stop_float);
+    Motor_Stop(Port_B, Motor_stop_float);
+}
+
+void driveForward (int delay) {
+    Motor_Drive(Port_A, Motor_dir_forward, 75);
+    Motor_Drive(Port_B, Motor_dir_forward, 75);
+    Delay(delay);
+    Motor_Stop(Port_A, Motor_stop_float);
+    Motor_Stop(Port_B, Motor_stop_float);
+}
+
+
 int main()
 {
-    sensor_touch_clicked_t touch;
-    SensorConfig(Port_1, SensorTouch);
-
-
     MotorPortInit(Port_A);
     MotorPortInit(Port_B);
 
-    NNXT_LCD_DisplayStringAtLine(0, "Linken sensor drücken");
 
-// wait for touch sensor to be clicked
-    while (1)
+    for (int i = 11; i >= 0; i--)
     {
-        Touch_Clicked(Port_1, &touch);
-        if (touch == SensorTouch_clicked)
-        {
-            break;
-        }
-        Delay(100);
-    }
-
-    while (1)
-    {
-        Motor_Drive(Port_A, Motor_dir_forward, 75);
-        Motor_Drive(Port_B, Motor_dir_backward, 75);
-        Delay(500);
-        Motor_Stop(Port_A, Motor_stop_float);
-        Motor_Stop(Port_B, Motor_stop_float);
-        Delay(1000);
-        Motor_Drive(Port_A, Motor_dir_backward, 75);
-        Motor_Drive(Port_B, Motor_dir_forward, 75);
-        Delay(500);
-        Motor_Stop(Port_A, Motor_stop_float);
-        Motor_Stop(Port_B, Motor_stop_float);
-        Delay(1000);
+        driveForward ((i + 5)* 100);
+        Delay(50);
+        turnLeft();
+        Delay(50);
     }
     return 0;
 }
+
+
+
