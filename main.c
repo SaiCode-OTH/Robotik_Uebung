7581437c5cce0f827914e92587c06a06be35347e
= <jung.tobias-it21@it.dhbw-ravensburg.de>
2024-04-23 15:22:54 +0200

    funktioniert?!

--- main.c
+++ main.c
@@ -1,21 +1,21 @@
 #include "nnxt.h"
 #include "stdbool.h"
 
-#define DEG_TO_RPS (166.6667/60)
+#define DEG_TO_RPS (166.6667 / 60)
 #define WHEEL_DIAMETER 4.3
 #define PI 3.14159
 
 #define TARGET_SPEED 9.0
-#define KP 10
-#define KI 1.5
+#define KP 20
+#define KI 0.01
 #define KD 0.75
-#define DELAY 100
+#define DELAY 50
 
 #define MOTOR Port_A
 
 uint32_t speed_last_deg_measure, speed_last_time_measure, last_i_time, last_d_delta;
-double rps, cummulativeDelta;
-
+double rps;
+double cumulativeDelta = 0.0;
 
 void getCurrentSpeed(double *speedInCmPerS)
 {
@@ -36,29 +36,41 @@ double pRegulator(double diff)
 
 double iRegulator(double diff)
 {
-    cummulativeDelta += diff;
-    int8_t ret = KI * cummulativeDelta * DELAY;
+    if (diff != diff || diff == 0)
+    {
+        return 0;
+    }
+    cumulativeDelta = cumulativeDelta + diff;
+    char dispMsg[20];
+    sprintf(dispMsg, "i: %f    ", cumulativeDelta);
+    NNXT_LCD_DisplayStringAtLine(3, dispMsg);
+    double ret = KI * cumulativeDelta * DELAY;
     last_i_time = GetSysTime();
+
+    sprintf(dispMsg, "i: %f    ", ret);
+    NNXT_LCD_DisplayStringAtLine(6, dispMsg);
     return ret;
 }
 
 double dRegulator(double diff)
 {
-    int8_t ret = KD * (diff - last_d_delta) / DELAY;
+    double ret = KD * (diff - last_d_delta) / DELAY;
     last_d_delta = diff;
+    char dispMsg[20];
+    sprintf(dispMsg, "d: %f    ", ret);
+    NNXT_LCD_DisplayStringAtLine(7, dispMsg);
     return ret;
 }
 
-
 int main()
 {
-    int8_t motorForce;
+    int16_t motorForce;
     double currentSpeed, ds;
     char dispMsg[20];
 
     Motor_Tacho_GetCounter(MOTOR, &speed_last_deg_measure);
     last_i_time = speed_last_time_measure = GetSysTime();
-    last_d_delta = speed_last_deg_measure = motorForce = cummulativeDelta = 0;
+    last_d_delta = speed_last_deg_measure = motorForce  = 0;
     while (1)
     {
         // calculate speed difference
@@ -66,24 +78,24 @@ int main()
         sprintf(dispMsg, "sp: %f    ", currentSpeed);
         NNXT_LCD_DisplayStringAtLine(0, dispMsg);
         ds = TARGET_SPEED - currentSpeed;
-        
+
         sprintf(dispMsg, "ds: %f    ", ds);
         NNXT_LCD_DisplayStringAtLine(1, dispMsg);
         // call regulators
-        motorForce = pRegulator(ds) + iRegulator(ds) + dRegulator(ds);
+        motorForce = (pRegulator(ds) + iRegulator(ds) + dRegulator(ds));
         // cap motor force if above 100
-        if (motorForce > 100) {
+        if (motorForce > 100)
+        {
             motorForce = 100;
         }
-        else if (motorForce < 0) {
+        else if (motorForce < 0)
+        {
             motorForce = 0;
         }
-
 
         sprintf(dispMsg, "force: %d    ", motorForce);
         NNXT_LCD_DisplayStringAtLine(2, dispMsg);
 
-        
         // apply motor force
         Motor_Drive(MOTOR, Motor_dir_forward, motorForce);
         Delay(DELAY);
