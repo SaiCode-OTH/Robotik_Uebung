a13e50626b03d50c696df38635592470b2b65ce6
= <jung.tobias-it21@it.dhbw-ravensburg.de>
2024-05-14 13:15:35 +0200

    welcome to hell

--- main.c
+++ main.c
@@ -2,13 +2,16 @@
 #define IR_SENSOR Port_3
 
 const int16_t correctionPoints[][2] = {
+    {50, 800},
+    {60, 700},
     {78, 600},
     {85, 500},
     {106, 400},
     {137, 300},
     {180, 200},
     {270, 100},
-    {615, 0}};
+    {615, 0}
+    };
 
 const uint8_t correctionPointsLength = sizeof(correctionPoints) / sizeof(correctionPoints[0]);
 
@@ -32,7 +35,7 @@ uint16_t calcDistance(uint16_t distance)
     uint8_t lowerIndex = index - 1;
     int16_t slope = ((int32_t)(correctionPoints[index][1] - correctionPoints[lowerIndex][1]) * 1000) / (correctionPoints[index][0] - correctionPoints[lowerIndex][0]);
 
-    sprintf(msg, " index: %d  | slope: %d  -> res: %d   \n", index, slope, (correctionPoints[lowerIndex][1] * 1000 + slope * (distance - correctionPoints[lowerIndex][0])) / 1000);
+    sprintf(msg, " index: %d | slope: %d  -> res: %d   \n", index, slope, (correctionPoints[lowerIndex][1] * 1000 + slope * (distance - correctionPoints[lowerIndex][0])) / 1000);
     printf(msg);
     // Linear interpolation
     return (correctionPoints[lowerIndex][1] * 1000 + slope * (distance - correctionPoints[lowerIndex][0])) / 1000;
